298ab54e79fa08b7ac0a7fd24854b0090f15b4b9
segyhp <segyhendropratama@gmail.com>
2020-01-25 21:52:37 +0700

    feat: create sample response

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+
+let app = express();
+
+app.get('/', (req, res) => {
+    // res.send('<h1>Hello Express!</h1>');
+    res.send(
+        {
+            'name': 'Segy',
+            'age': 25,
+            'isMarried': false,
+            games: ['dota 2', 'the witcher 3']
+        }
+    );
+});
+
+app.get('/about', (req, res) => {
+    res.send('<h2>About Page </h2>');
+});
+
+app.get('/bad', (req, res) => {
+   res.send({
+       status: 404,
+       message: 'Content not Found!'
+   })
+});
+
+app.listen(3000);
