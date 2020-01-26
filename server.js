727cf5f5813c27cce7dfd7f917b1d0faccd7a0f8
segyhp <segyhendropratama@gmail.com>
2020-01-26 18:44:45 +0700

    feat: use template engine to render data in template

--- server.js
+++ server.js
@@ -1,23 +1,34 @@
 const express = require('express');
+const hbs = require('hbs');
 
 let app = express();
 
+app.set('view engine', 'hbs');
 app.use(express.static(`${__dirname}/public`));
 
 app.get('/', (req, res) => {
     // res.send('<h1>Hello Express!</h1>');
-    res.send(
-        {
-            'name': 'Segy',
-            'age': 25,
-            'isMarried': false,
-            games: ['dota 2', 'the witcher 3']
-        }
-    );
+    // res.send(
+    //     {
+    //         'name': 'Segy',
+    //         'age': 25,
+    //         'isMarried': false,
+    //         games: ['dota 2', 'the witcher 3']
+    //     }
+    // );
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        welcomeMessage: 'Welcome to the page',
+        currentYear: new Date().getFullYear()
+    })
 });
 
 app.get('/about', (req, res) => {
-    res.send('<h2>About Page </h2>');
+    // res.send('<h2>About Page </h2>')
+    res.render('about.hbs', {
+        pageTitle: 'About Page',
+        currentYear: new Date().getFullYear()
+    });
 });
 
 app.get('/bad', (req, res) => {
